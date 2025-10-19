d2e4d32e3d3ca805fc774e5aa1b7d67e87cee51b
anduimagui <andrew@happysoft.dev>
2025-10-19 07:27:54 +0100

    Fix Homebrew formula template and improve sed precision

--- Formula/mailos.rb
+++ Formula/mailos.rb
@@ -1,18 +1,18 @@
 class Mailos < Formula
   desc "Command-line email client powered by AI"
   homepage "https://email-os.com"
-  url "https://github.com/anduimagui/emailos-cli/archiv0.1.37e/refs/tags/v0.1.37tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/anduimagui/emailos-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
   license "Proprietary"
   head "https://github.com/anduimagui/emailos-cli.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.v0.1.37ersion=#{v0.1.37ersion}")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
   end
 
   test do
-    assert_match v0.1.37ersion.to_s, shell_output("#{bin}/mailos --v0.1.37ersion 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/mailos --version 2>&1")
   end
 end
